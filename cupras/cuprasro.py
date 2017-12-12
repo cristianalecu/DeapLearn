e33fde11789a4d59dd95c540f527d46aa9e37415
Cristian Alecu <cristian.alecu@pss.ro>
2017-12-12 10:35:21 +0200

    bkk

--- cupras/cuprasro.py
+++ cupras/cuprasro.py
@@ -23,46 +23,64 @@ if not 'last_date' in mem.keys():
     mem["last_date"] = '2005-01-01'
 if not 'monezi' in mem.keys():
     mem["monezi"] = {}
-    
-mem["last_date"] = datetime.datetime.strptime(mem["last_date"], "%Y-%m-%d")    #.strftime("%Y-%m-%d") 
-try:
-    r = requests.get("https://www.cursbnr.ro/arhiva-curs-bnr-2005-01-03"
-        #, proxies=dict(http="http://cna:6yuiop[]@192.168.1.29:8080", https="https://cna:6yuiop[]@192.168.1.29:8080")
-        )
-except ConnectionError as e:
-    print ("HTTP Connection Error")
-    r = None
 
-if not r == None:
-    if r.status_code == 200:  # Success
-        soup = BeautifulSoup(r.text, "html.parser")
-        for table in soup.find_all("table", "table-striped-archive"):
-            d={'Data': [], 'Valoare': [], "Crestere":[], "Predicts":[]}
-            c={'idx':0, 'Simbol':'', 'Name':'', 'Country':'', 'Data_from':'', 'Data_to':'', 'days_predicted':1, 'spread':0, 'success_rate':0, 'max_devia_allow':0, 'avg_devia':0}
-            for row in  table.find_all("tr"):
-                heads=row.find_all("th")
-                cells = row.find_all("td")
-                if len(heads) > 0:
-                    date = datetime.datetime(heads[2].text)
-                elif len(cells) > 0:
-                    symb = cells[0].text
-                    den = cells[1].text
-                    val = float(cells[2].text)
-                    inc = float(cells[3].text)
-                if symb in mem["monezi"].keys:
-                    m = mem["monezi"][symb]
-                    c=m["info"]
-                    d=m["data"]
-                else:
-                    d={'Data': [], 'Valoare': [], "Crestere":[], "Predicts":[]}
-                    c={'idx':0, 'Simbol':symb, 'Name':den, 'Country':'', 'Data_from':date, 'Data_to':date, 'days_predicted':1, 'spread':0, 'success_rate':0, 'max_devia_allow':0, 'avg_devia':0}
-                    m = {"info":c, "data":d}
-                    mem["monezi"][symb] = m
-                d["Data"].append(date)
-                d["Valoare"].append(val)
-                d["Crestere"].append(inc)
-                c["Data_to"] = date
-        
+dates = []    
+today = datetime.datetime.now().strftime("%Y-%m-%d") 
+if today > mem["last_date"]:
+    try:
+        r = requests.get("https://www.cursbnr.ro/arhiva-curs-bnr"
+            , proxies=dict(http="http://cna:6yuiop[]@192.168.1.29:8080", https="https://cna:6yuiop[]@192.168.1.29:8080")
+            )
+    except ConnectionError as e:
+        print ("HTTP Connection Error")
+        r = None
+    
+    if not r == None:
+        if r.status_code == 200:  # Success
+            soup = BeautifulSoup(r.text, "html.parser")
+            for tag_date in soup.find_all("a", "arhiva_btn"):
+                date = tag_date.attrs['href'][16:]
+                if date > mem["last_date"]:
+                    dates.append(date)
+dates.sort()    
+for datex in dates:
+    try:
+        r = requests.get("https://www.cursbnr.ro/arhiva-curs-bnr-"+datex
+            , proxies=dict(http="http://cna:6yuiop[]@192.168.1.29:8080", https="https://cna:6yuiop[]@192.168.1.29:8080")
+            )
+    except ConnectionError as e:
+        print ("HTTP Connection Error")
+        r = None
+        break;
+    
+    if not r == None:
+        if r.status_code == 200:  # Success
+            soup = BeautifulSoup(r.text, "html.parser")
+            for table in soup.find_all("table", "table-striped-archive"):
+                for row in  table.find_all("tr"):
+                    heads=row.find_all("th")
+                    cells = row.find_all("td")
+                    if len(heads) > 0:
+                        date = datex
+                    elif len(cells) > 0:
+                        symb = cells[0].text
+                        den = cells[1].text
+                        val = float(cells[2].text)
+                        inc = float(cells[3].text)
+                        if symb in mem["monezi"].keys():
+                            m = mem["monezi"][symb]
+                            c=m["info"]
+                            d=m["data"]
+                        else:
+                            d={'Data': [], 'Valoare': [], "Crestere":[], "Predicts":[]}
+                            c={'idx':0, 'Simbol':symb, 'Name':den, 'Country':'', 'Data_from':date, 'Data_to':date, 'days_predicted':1, 'spread':0, 'success_rate':0, 'max_devia_allow':0, 'avg_devia':0}
+                            m = {"info":c, "data":d}
+                            mem["monezi"][symb] = m
+                        d["Data"].append(date)
+                        d["Valoare"].append(val)
+                        d["Crestere"].append(inc)
+                        c["Data_to"] = date
+            mem["last_date"] = datex
 with open('cupro.pickle', 'wb') as f:
     pickle.dump(mem, f)
     
