6e70dcf92e88432320d4b68e998c6ccf1b8462b3
Cristian Alecu <cristian.alecu@gmail.com>
2018-05-18 15:08:07 +0300

    upd

--- MachineLearn/env.bat
+++ MachineLearn/env.bat
@@ -9,14 +9,13 @@ call python -m venv  venv_dml
 
 cd ..\DeapLearn\MachineLearn\
 
-
-
 :NO_CREATE
-
 
 call ..\..\venv\venv_dml\Scripts\activate.bat  
 
-set https_proxy=http://192.168.1.29:8080
+echo "Use env r   to install requirements"
+echo "Use env r p  if you have also pss proxy "
 
+if %computername%==L560-CNA if [%2]==[p] set https_proxy=http://192.168.1.29:8080
 
 if [%1] == [r] call pip install -r requirements.txt
